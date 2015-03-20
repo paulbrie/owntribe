e669f2413908545f43207b5cccae4fe1d2c22266
Paul Brie <pbrie@sdl.com>
2015-03-20 09:42:00 +0200

    ignored .idea

--- owntribe/classes/Base.php
+++ owntribe/classes/Base.php
@@ -5,6 +5,7 @@ namespace owntribe\classes {
      * This class is defining the low level capabilities of all other classes of the package owntribe\classes
      *
      * @package owntribe\classes
+     * @author  Paul BRIE
      */
     class Base {
         use \owntribe\traits\Response;
