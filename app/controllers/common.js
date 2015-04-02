1a2f0733e066c9c5cc868a8acd175499db2586c0
Paul Brie <pbrie@sdl.com>
2015-04-02 14:22:30 +0300

    tasks seen only by owner + bug fix

--- app/controllers/common.js
+++ app/controllers/common.js
@@ -2,13 +2,10 @@ var tasks = require('../models/tasks');
 module.exports = function(app) {
     return {
         indexPage: function(req, res, next) {
-            tasks.getTasks(function(rows){
-                //console.log(rows);
-                res.render('home', {
-                    title: 'Owntribe',
-                    h1: 'Owntribe, the Private Open Source Social Network',
-                    menu_active_home: ' class="active" '
-                });
+            res.render('home', {
+                title: 'Owntribe',
+                h1: 'Owntribe, the Private Open Source Social Network',
+                menu_active_home: ' class="active" '
             });
         }
     };
