840c567789b23f2899daf687d5c43a01418a02c8
Paul Brie <pbrie@sdl.com>
2015-04-17 21:09:26 +0300

    clean master

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = array(
-    'dbname' => 'owntribe',
-    'dbuser' => 'owntribe',
-    'dbpass' => 'owntribe',
-    'debug' => true,
-    'start' => microtime(true)
-);
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'config.php';
-include 'helpers.php';
-$Main = new \owntribe\classes\Main($config);
