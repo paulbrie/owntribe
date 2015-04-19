2263a8bd5fc69c321a653222e3adeb264599323f
Mihai Sampaleanu <msampaleanu@sdl.com>
2015-04-19 12:39:21 +0200

    update db scripts add create script

--- app/install/sql/users.sql
+++ app/install/sql/users.sql
@@ -1,12 +1,10 @@
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
   `email` varchar(255) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
   `type` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (id)
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-ALTER TABLE `owntribe`.`users`
-ADD COLUMN `fname` VARCHAR(45) NULL AFTER `id`,
-ADD COLUMN `lname` VARCHAR(45) NULL AFTER `fname`,
-ADD COLUMN `created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP AFTER `type`;
